922cdb89c9ddfc2a9e941e14b6f82737a196b4f3
MariaSw <kryvunmaria@gmail.com>
2017-03-02 16:34:02 +0400

    на интерфейсы

---
+++ src/com/javarush/task/task12/task1217/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task12.task1217;
+
+/* 
+Интерфейсы Fly, Run, Swim
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //add interfaces here - добавь интерфейсы тут
+public interface Swim{
+        void swim(String style);
+    }
+public interface Run{
+        void run(int speed);
+}
+public interface Fly{
+        void fly(int height);
+}
+}
