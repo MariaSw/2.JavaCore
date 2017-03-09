161ed3427be3a170866ed5b921fa48b61cd06f13
MariaSw <kryvunmaria@gmail.com>
2017-03-09 18:17:51 +0400

    на абстрактные классы

---
+++ src/com/javarush/task/task13/task1313/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task13.task1313;
+
+import java.awt.*;
+
+/* 
+Интерфейс Animal
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+    }
+    public interface Animal
+    {
+        Color getColor();
+    }
+    public static abstract class Fox implements Animal//сделали класс абстрактным, значит можно не реализовывать метод интерфейса
+    {
+        public String getName()
+        {
+            return "Fox";
+        }
+    }
+}
