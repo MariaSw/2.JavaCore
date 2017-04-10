7d11941555c763ea565fce1fee72943c6bedbc82
MariaSw <kryvunmaria@gmail.com>
2017-04-10 21:43:57 +0400

    Финальная версия

--- src/com/javarush/task/task14/task1420/Solution.java
+++ src/com/javarush/task/task14/task1420/Solution.java
@@ -4,6 +4,7 @@ package com.javarush.task.task14.task1420;
 НОД
 */
 
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -14,64 +15,60 @@ public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-
             String numS1 = reader.readLine();
-        if (!numS1.matches("\\d+"))
-            throw new NumberFormatException();
-
             String numS2 = reader.readLine();
-        if (!numS2.matches("\\d+"))
-            throw new NumberFormatException();
 
-            int num1 = Integer.parseInt(numS1);
-            int num2 = Integer.parseInt(numS2);
+            if(isValid(numS1)&&isValid(numS2))
+            System.out.println(findNOD(findDublicates(findDel(Integer.parseInt(numS1))),findDublicates(findDel(Integer.parseInt(numS2)))));
 
-            System.out.println(findNOD(findDublicates(findDel(num1)),findDublicates(findDel(num2))));
-
+    }
+    public static boolean isValid (String num){
+        if (!num.matches("\\d+")||num.charAt(0)=='0')
+            throw new NumberFormatException("Введите два целых положительных числа.");
+        else return true;
     }
     public static ArrayList findDel(int num1) {
-        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
 
         for (int i = 2; i <= num1; i++) {
-            while (true) {
-                if (num1 % i == 0) {
-                    list1.add(i);
-                    num1 = num1 / i;
-                    }else break;
+            while (true) {/**цикл внутри цикла неободим для мнократного использования одного и то же числа*/
+                if (num1 % i == 0) {/**если исходное число делится на предложенное число без остатка, */
+                    list.add(i);/**добавляем его в список*/
+                    num1 = num1 / i;/**делим на предложенное число, переопределяем исходное число для поиска последующих делителей*/
+                    }else break;/**в случае, если исходное число не делится без остатка, выходим из цикла, пробуем делить на след. число*/
             }
-            if (num1 == i) list1.add(num1);
         }
-        return list1;
+        return list;
     }
-        public static HashMap findDublicates(ArrayList<Integer> list1) {
+        public static HashMap findDublicates(ArrayList<Integer> list) {
 
-        HashMap<Integer, Integer> puth = new HashMap<>();
+        HashMap<Integer, Integer> puthere = new HashMap<>();/**карта для перекладывания чисел с целью нахождения каоличества повторов каждого числа*/
 
-            for (int i = 0; i < list1.size(); i++){
-                if (puth.containsKey(list1.get(i))){
-                    for (Map.Entry<Integer,Integer> pair:puth.entrySet()){
-                      if (list1.get(i)==pair.getKey())
-                          pair.setValue(pair.getValue()+1);
+            for (int i = 0; i < list.size(); i++){
+                if (puthere.containsKey(list.get(i))){/**2. Если уже содержит*/
+                    for (Map.Entry<Integer,Integer> pair:puthere.entrySet()){
+                      if (list.get(i)==pair.getKey())/**ищем такое число в карте*/
+                          pair.setValue(pair.getValue()+1);/**меняем увеличиваем счетчик на 1*/
                     }
                 }else{
-                    puth.put(list1.get(i), 1);
+                    puthere.put(list.get(i), 1);/**1. Если карта не содержит такое число, кладем, ставим счетчик - 1*/
                 }
             }
-        return puth;
+        return puthere;
         }
 
         public static int findNOD(HashMap<Integer,Integer> map1,HashMap<Integer, Integer> map2) {
-        int NOD = 1;
+        int NOD = 1;/**ставим дефолтом 1, если ни один делитель не совпадает*/
 
             for (Map.Entry<Integer, Integer> pair1:map1.entrySet()){
                 for (Map.Entry<Integer, Integer> pair2:map2.entrySet()){
-                    if(pair1.getKey()==pair2.getKey()){
-                        if (pair1.getValue()>=pair2.getValue()) {
-                            NOD = NOD * (int) Math.pow(pair1.getKey(), pair2.getValue());
+                    if(pair1.getKey()==pair2.getKey()){/**сравниваем делители двух карт(чисел), на наличие одинаковых*/
+                        if (pair1.getValue()>=pair2.getValue()) {/**далее берем наименьший показатель счетчика повторов из двух карт */
+                            NOD = NOD * (int) Math.pow(pair2.getKey(), pair2.getValue());/**и возводим в такую степень*/
                         }
 
                         else
-                            NOD = NOD*(int)Math.pow(pair1.getKey(),pair1.getValue());
+                            NOD = NOD*(int)Math.pow(pair2.getKey(),pair1.getValue());
                     }
                 }
             }
